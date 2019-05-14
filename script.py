23b36cc7c924d06ca76de1a6249c20ba31e7abd2
DevMMing <mming@stuy.edu>
2019-05-14 11:02:28 -0400

    s

--- script.py
+++ script.py
@@ -11,11 +11,6 @@ def run(filename):
 
     if p:
         (commands, symbols) = p
-        #print(p)
-        #print("commands")
-        #print(commands)
-        print("symbols")
-        print(symbols)
     else:
         print ("Parsing failed.")
         return
@@ -51,6 +46,61 @@ def run(filename):
                           'blue': [0.2, 0.5, 0.5]}]
     reflect = '.white'
 
-    print (symbols)
+    #print (symbols)
     for command in commands:
-        print (command)
+    #    print (command)
+        if command["op"]=="move":
+            t = make_translate(float(command["args"][0]), float(command["args"][1]), float(command["args"][2]))
+            matrix_mult( stack[-1], t )
+            stack[-1] = [ x[:] for x in t]
+        elif command["op"]== "sphere":
+            add_sphere(polygons,
+                       float(command["args"][0]), float(command["args"][1]), float(command["args"][2]),
+                       float(command["args"][3]), step_3d)
+            matrix_mult( stack[-1], polygons )
+            draw_polygons(polygons, screen, zbuffer, view, ambient, light, areflect, dreflect, sreflect)
+            polygons = []
+        elif command["op"]=="rotate":
+            theta = float(command["args"][1]) * (math.pi / 180)
+            if args[0] == 'x':
+                t = make_rotX(theta)
+            elif args[0] == 'y':
+                t = make_rotY(theta)
+            else:
+                t = make_rotZ(theta)
+            matrix_mult( stack[-1], t )
+            stack[-1] = [ x[:] for x in t]
+        elif command["op"]=="box":
+            add_box(polygons,
+                    float(command["args"][0]), float(command["args"][1]), float(command["args"][2]),
+                    float(command["args"][3]), float(command["args"][4]), float(command["args"][5]))
+            matrix_mult( stack[-1], polygons )
+            if command["constants"]==None:
+                areflect=[[],,] 
+                draw_polygons(polygons, screen, zbuffer, view, ambient, light, areflect, dreflect, sreflect)
+            else:
+                draw_polygons(polygons, screen, zbuffer, view, ambient, light, areflect, dreflect, sreflect)
+            polygons = []
+        elif command["op"]=="pop":
+            stack.pop()
+        elif command["op"]=="push":
+            stack.append( [x[:] for x in stack[-1]] )
+        elif command["op"]=="scale":
+            t = make_scale(float(command["args"][0]), float(command["args"][1]), float(command["args"][2]))
+            matrix_mult( stack[-1], t )
+            stack[-1] = [ x[:] for x in t]
+        elif command["op"]=="torus":
+            add_torus(polygons,
+                      float(command["args"][0]), float(command["args"][1]), float(command["args"][2]),
+                      float(command["args"][3]), float(command["args"][4]), step_3d)
+            matrix_mult( stack[-1], polygons )
+            if command["constants"]==None:
+                draw_polygons(polygons, screen, zbuffer, view, ambient, light, areflect, dreflect, sreflect)
+            else:
+                draw_polygons(polygons, screen, zbuffer, view, ambient, light, areflect, dreflect, sreflect)
+            polygons = []
+        elif command["op"]=="display":
+            display(screen)
+        elif command["op"]=="save":
+            save_extension(screen, command["args"][0])
+            
